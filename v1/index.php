92064c423238b8e9c1a0021c8fcc4049dd9e7104
Bandana7 <112367587+Bandana7@users.noreply.github.com>
2023-08-03 16:41:02 +0545

    Add files via upload

---
+++ v1/index.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+if(isset($_SESSION["users"])) {
+  header("Location: login.php");
+} 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Cosplay Workshop</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+ 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+ 
+</head>
+<body>
+  <section id="header">
+    <a href="#"><img src="images/logo.png" height="75px" width="75px" alt="logo" class="logo"></a>
+    <a href="#"><img src="images/name.png" height="75px" width="200px" alt="logo" class="name"></a>
+    <div>
+      <ul id="navbar">
+        <li><a class="active" href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+       
+        <li><a href="login.php">Login</a></li>
+        <li><a href="registration.php">Register</a></li>
+       
+      </ul>
+    </div>
+  </section>
+
+  <section id="hero">
+    <h4> Trade-In-Offer!!!</h4>
+    <h2>Super Value Offer</h2>
+    <h1>On all products</h1>
+    <p>Buy 3 get 1 free.</p>
+    <button class="shop"> Shop Now</button>
+  </section>
+
+ 
+
+  
+
+
+ 
+
+  <footer class="section-p1">
+    <div class="col">
+      <h4> Contact</h4>
+      <p><strong>Phone: </strong>+9770000000000 </p>
+      <p><strong>Address: </strong> Thamel, Kathmandu</p>
+      <p><strong>Hours: </strong>10:00-18:00, Sun-Fri</p>
+      
+      <div class="follow">
+        <h4>Follow Us</h4>
+        <div class="icon">
+          <i class="fab fa-facebook-f"></i>
+          <i class="fab fa-instagram"></i>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="col install">
+      <h4>Install App</h4>
+      <p>From App Store or Google Play</p>
+      
+      <div class="row">
+        <img src="images/appstore.png" width="45%" height="35%" alt="">
+        <img src="images/google.png" width="45%" height="35%" alt="">
+      </div>
+      <p>Secured Payment Gateways</p>
+      <img src="images/pay.png" width="45%" height="35%" alt="">
+    </div>
+
+    <div class="copyright">
+      <p>&copy; 2023 My Website. All rights reserved.</p>
+    </div>
+    
+  </footer>
+<script src="script.js"></script>
+</body>
+</html>
