d4cc2f519f9c40234931b13bc6621d438bd32936
Bandana7 <112367587+Bandana7@users.noreply.github.com>
2023-08-04 11:54:46 +0545

    admin panel started

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<?php include('includes/header.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+ 
+</head>
+<body>
+    
+<div class="admin-container">
+        
+    <div class="row">
+        <div class="col-md-12">
+        <h3>Hi, <span>admin</span></h3>
+        <h1>Welcome</h1>
+        </div>
+    </div>
+</div>
+ <?php include('includes/footer.php');?>
+</body>
+</html>
